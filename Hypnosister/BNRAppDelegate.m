4afce24a6078ca542c476fa576e438b0ca9bb84d
rebornwhu <rebornwhu@gmail.com>
2015-05-09 16:10:11 -0700

    Enable paging

--- Hypnosister/BNRAppDelegate.m
+++ Hypnosister/BNRAppDelegate.m
@@ -18,15 +18,19 @@
     CGRect screenRect = self.window.bounds;
     CGRect bigRect = screenRect;
     bigRect.size.width *= 2.0;
-    bigRect.size.height *= 2.0;
     
     UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:screenRect];
+    scrollView.pagingEnabled = YES;
     [self.window addSubview:scrollView];
     
-    BNRHypnosisView *hyposisView = [[BNRHypnosisView alloc] initWithFrame:bigRect];
+    BNRHypnosisView *hyposisView = [[BNRHypnosisView alloc] initWithFrame:screenRect];
     [scrollView addSubview:hyposisView];
     
+    screenRect.origin.x += screenRect.size.width;
+    
     scrollView.contentSize = bigRect.size;
+    BNRHypnosisView *anotherView = [[BNRHypnosisView alloc] initWithFrame:screenRect];
+    [scrollView addSubview:anotherView];
     
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
